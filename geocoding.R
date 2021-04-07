3e821862e5db90b9ab0d03eb53f0ba8e5340c50a
Benjamin Flückiger <fluben00@STI.local>
2021-04-07 15:33:26 +0200

    adapt stuff

--- geocoding.R
+++ geocoding.R
@@ -8,8 +8,8 @@
 #address = string with all address information
 #sleep = delay, if set to 3 seconds (default) not more than 20 requests per minute => FAIR USE regulations of API
 #gives several addresses if not precise (the lower the weight, the better the match)
-library(jsonlite)
-library(httr)
+#library(jsonlite)
+#library(httr)
 
 geocoding <- function(address, sleep=3){
   if(is.character(address)==FALSE){
@@ -17,21 +17,21 @@ geocoding <- function(address, sleep=3){
   } 
   if(sleep<3) warning('too many requests per minute')
   path="https://api3.geo.admin.ch/rest/services/api/SearchServer?"
-  request <- GET(url = path, 
+  request <- httr::GET(url = path, 
                  query = list(
                    searchText = address,
                    origins= "address",
                    type = "locations")
   )
-  response <- content(request, as = "text", encoding = "UTF-8")
+  response <- httr::content(request, as = "text", encoding = "UTF-8")
   #if no result give warning
   if(response == "{\"fuzzy\":\"true\",\"results\":[]}") warning(paste0("no result found for ", address))
   #if no result return empty data frame
   if (response == "{\"fuzzy\":\"true\",\"results\":[]}"){
     df <- data.frame()
   } else {
-    df <- data.frame(fromJSON(response, flatten = TRUE)) 
-    df$status_code <- status_code(request) #200=all fine
+    df <- data.frame(jsonlite::fromJSON(response, flatten = TRUE)) 
+    df$status_code <- httr::status_code(request) #200=all fine
     df$get_request <- address
     #delete fuzzy column if exists
     if("fuzzy" %in% colnames(df))
